aafc4a61f57c9779fab9e554dd8b9b0cd404dac0
OliviaSapp2 <osapp2@uw.edu>
2024-09-25 14:19:00 -0700

    Hello World project runs on emulator

---
+++ HelloAndroid/app/src/main/java/edu/uw/ischool/osapp2/hello/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.uw.ischool.osapp2.hello
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.uw.ischool.osapp2.hello.ui.theme.HelloAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "GO DAWGS!",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = name,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloAndroidTheme {
+        Greeting("GO DAWGS!")
+    }
+}
